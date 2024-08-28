839ba1f4017d19eea45475ccd14fd7d6b8d9e9c6
Sandra Karime Gutiérrez Martínez <sandra.gutierrez@logrand.local>
2024-08-28 00:25:44 -0600

    thoughtform file

---
+++ client/src/components/ThoughtForm/index.jsx
@@ -0,0 +1,101 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/client';
+
+import { ADD_THOUGHT } from '../../utils/mutations';
+import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
+
+import Auth from '../../utils/auth';
+
+const ThoughtForm = () => {
+  const [thoughtText, setThoughtText] = useState('');
+
+  const [characterCount, setCharacterCount] = useState(0);
+
+  const [addThought, { error }] = useMutation
+  (ADD_THOUGHT, {
+    refetchQueries: [
+      QUERY_THOUGHTS,
+      'getThoughts',
+      QUERY_ME,
+      'me'
+    ]
+  });
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const { data } = await addThought({
+        variables: {
+          thoughtText,
+          thoughtAuthor: Auth.getProfile().data.username,
+        },
+      });
+
+      setThoughtText('');
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    if (name === 'thoughtText' && value.length <= 280) {
+      setThoughtText(value);
+      setCharacterCount(value.length);
+    }
+  };
+
+  return (
+    <div>
+      <h3>What's sparking your techy thoughts? Share what you love about StallCourt®!</h3>
+
+      {Auth.loggedIn() ? (
+        <>
+          <p
+            className={`m-0 ${
+              characterCount === 280 || error ? 'text-danger' : ''
+            }`}
+          >
+            Word Count: {characterCount}/280
+          </p>
+          <form
+            className="flex-row justify-center justify-space-between-md align-center"
+            onSubmit={handleFormSubmit}
+          >
+            <div className="col-12 col-lg-9">
+              <textarea
+                name="thoughtText"
+                placeholder="Here's a new thought..."
+                value={thoughtText}
+                className="form-input w-100"
+                style={{ lineHeight: '1.5', resize: 'vertical' }}
+                onChange={handleChange}
+              ></textarea>
+            </div>
+
+            <div className="col-12 col-lg-3">
+              <button className="btn btn-primary btn-block py-3" type="submit">
+              Share a Spark
+              </button>
+            </div>
+            {error && (
+              <div className="col-12 my-3 bg-danger text-white p-3">
+                {error.message}
+              </div>
+            )}
+          </form>
+        </>
+      ) : (
+        <p>
+          Log in to light up StallCourt® with your thoughts. Please{' '}
+          <Link to="/login">login</Link> or <Link to="/signup">signup.</Link>
+        </p>
+      )}
+    </div>
+  );
+};
+
+export default ThoughtForm;
